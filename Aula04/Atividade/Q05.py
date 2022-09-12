36b87ddc45b75401b617a4b66fd40de33fbbc06f
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2022-09-12 19:51:10 -0300

    Create Q05.py

---
+++ Aula04/Atividade/Q05.py
@@ -0,0 +1,6 @@
+num1=int(input("Digite um valor: "))
+num2=int(input("Digite um valor diferente do primeiro: "))
+if num1>num2:
+    print(f'O primeiro valor é maior do que o segundo! {num1}>{num2}')
+else:
+    print(f'O segundo valor é maior do que o primeiro! {num2}>{num1}')
