9d8a59d33ea8806bad8832ce91b3d70ef662cbcf
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2022-09-13 20:50:24 -0300

    Create Q02.py

---
+++ Aula06/Atividade/Q02.py
@@ -0,0 +1,8 @@
+'''Faça um programa que mostre as tabuadas dos números de 1 a 10.
+'''
+
+for num in range(1, 11):
+    print(f"Tabuada da {num}")
+    
+    for num2 in range(1, 11):
+        print(f'{num} x {num2}')
