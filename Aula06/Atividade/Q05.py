731ce0aa9bc245f05058712d0e881d9e55d60236
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2022-09-14 19:51:48 -0300

    Create Q05.py

---
+++ Aula06/Atividade/Q05.py
@@ -0,0 +1,10 @@
+'''Utilizando a lista do exercício anterior, exiba uma saudação ("Olá, como vai você"), 
+personalizado com o nome de cada amigo. A saudação deve ser a mesma, alterando apenas o nome do amigo.
+'''
+
+
+lista_amigos = [
+    "Gabriel", "Luiz", "Serginho", "João"]
+
+for nome in lista_amigos:
+    print(f"Olá, {nome}. Como vai você?")
