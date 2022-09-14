4838a0fe2059411ae421ce47ff9de74bc8f1d34a
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2022-09-13 21:01:48 -0300

    Create Q03.py

---
+++ Aula06/Atividade/Q03.py
@@ -0,0 +1,7 @@
+'''Armazene os nomes de alguns de seus amigos em uma lista chamada amigos. Exiba o nome de cada
+pessoa acessando cada elemento da lista um de cada vez.
+'''
+lista_amigos = ['Gabriel','Luiz','Sergio','João']
+
+for nome in lista_amigos:
+    print(f'{nome}, Obrigador por vir a aula!')
