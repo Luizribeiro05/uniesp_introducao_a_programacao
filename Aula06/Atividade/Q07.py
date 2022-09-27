5da32700c1151f0bdd71387430969021f72e7e25
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2022-09-27 19:58:08 -0300

    Update Q07.py

--- Aula06/Atividade/Q07.py
+++ Aula06/Atividade/Q07.py
@@ -1,14 +1,29 @@
-'''Faça um cadastro de usuários com nome, idade e email, utilizando apenas o que você aprendeu até agora.
+'''Faça um cadastro de usuários com nome, idade e email, 
+utilizando apenas o que você aprendeu até agora.
 '''
+cadastro=[]
 
+botao = 1000
+while botao != 0:
+    print("Digite 1 para cadastrar um novo usuario")
+    print("Digite 2 para imprimir todos os usuarios")
+    print("Digite 0 para sair")
+    botao = int(input("digite a opção desejada: "))
 
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-email = input("Digite seu email: ")
+    if botao == 1:
+        nome = input("Digite o nome: ")
+        idade = int(input("digite a idade: "))
+        email = input("Digite o e-mail: ")
+        dados = [nome,idade,email]
+        cadastro.append(dados)
+    
+    
+    elif botao == 2:
+        for p in cadastro:
+            print(p)
+    
+    elif botao == 0:
+        print("Obrigado Por acessar este software !")
 
-pessoa = nome, idade, email
-
-lista = [pessoa]
-
-for pessoa in lista:
-    print(f"Nome:{nome}, idade:{idade}, email:{email}. Inscrição bem sucedida.")
+    else:
+        print("Digite um numero válido!")
