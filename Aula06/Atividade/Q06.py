350849926b1fef72c3cf4e12903f794a6e55fb5b
Luiz Gonzaga <112145713+Luizribeiro05@users.noreply.github.com>
2022-09-27 19:35:53 -0300

    Update Q06.py

--- Aula06/Atividade/Q06.py
+++ Aula06/Atividade/Q06.py
@@ -1,26 +1,18 @@
-'''Crie uma lista de convidados para um jantar em sua casa, com pelo menos 5 celebridades.
-'''
+'''Lista de Convidados'''
+convidados = ["Ronaldinho Gaúcho", "Jack Chan", "Orochi", "Lionel Messi", "Tupac Sharkur"]
 
-lista_celebridades = ["Ronaldinho Gaúcho", "Jack Chan", "Orochi", "Lionel Messi", "Tupac Sharkur"]
+'''Convite '''
+for nome in convidados:
+    print(f"Bora pra balada, {nome}!")
 
-'''Envie um convite para cada uma dessas pessoas. Com a mesma mensagem e nome personalizado.
-'''
+'''Quem não poderar ir '''
+print("Jack Chan: Não vou !")
+print("Orochi: Não vou !")
 
-for nome in lista_celebridades:
-    print(f"{nome},Convite para festa no Local : Hotel California,420.Domingo,04/02, às 22 horas")
+'''Modifique sua lista, substitua os desistentes por novos convidados'''
 
-'''Sabendo que uma dessas pessoas não poderá ir ao seu jantar, você deverá enviar novos convites. Imprima o nome das pessoas que não poderão comparecer.
-'''
+convidados[1] = "Bob"
+convidados[2] = "Lula"
 
-print("Jack Chan e Orochi não poderão ir ao jantar")
-
-'''Modifique sua lista, substitua os desistentes por novos convidados.
-'''
-
-nova_lista_celebridades = ["Ronaldinho Gaúcho", "Eminem", "Bruce Lee", "Lionel Messi", "Tupac Sharkur"]
-
-'''Exiba um novo convite para cada pessoa que continua presente em sua lista.
-'''
-
-for nome in nova_lista_celebridades:
-    print(f"{nome},Convite para festa no Local : Hotel California,420.Domingo,04/02, às 22 horas")
+for nome in convidados:
+    print(f"Bora pra balada, {nome}!")
